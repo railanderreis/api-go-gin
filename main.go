75edb169bf19470cb7b3933bcfda0919145589c2
Railander Soares Alcantara dos Reis - TI Infraestrutura <railander.reis@redemeridional.com.br>
2023-09-25 13:09:47 -0300

    iniciando api com gin

---
+++ main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+)
+
+func ExibeTodosAlunos(c *gin.Context) {
+	c.JSON(200, gin.H{
+		"id":   "1",
+		"nome": "reis",
+	})
+}
+
+func main() {
+	r := gin.Default()
+	r.GET("/alunos", ExibeTodosAlunos)
+	r.Run()
+}
