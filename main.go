b71890b2a4cfc09286ee89141befb96256e95b08
Railander Soares Alcantara dos Reis - TI Infraestrutura <railander.reis@redemeridional.com.br>
2023-09-27 17:13:45 -0300

    add delete, editar e buscar

--- main.go
+++ main.go
@@ -2,16 +2,10 @@ package main
 
 import (
 	"github.com/railanderreis/api-go-gin/database"
-	"github.com/railanderreis/api-go-gin/models"
 	"github.com/railanderreis/api-go-gin/routes"
 )
 
 func main() {
 	database.ConectaComBancoDeDados()
-	models.Alunos = []models.Aluno{
-		{Nome: "reis 1", CPF: "123", RG: "123"},
-		{Nome: "reis 2", CPF: "123", RG: "123"},
-		{Nome: "reis 3", CPF: "123", RG: "123"},
-	}
 	routes.HandleRequests()
 }
--- routes/routes.go
+++ routes/routes.go
@@ -8,7 +8,10 @@ import (
 func HandleRequests() {
 	r := gin.Default()
 	r.GET("/alunos", controllers.ExibeTodosAlunos)
-	r.GET("/:nome", controllers.Saudacao)
-	r.POST("/aluno", controllers.CriaNovoAluno)
+	r.POST("/alunos", controllers.CriaNovoAluno)
+	r.GET("/alunos/:id", controllers.BuscaAlunoPorID)
+	r.DELETE("/alunos/:id", controllers.DeletaAluno)
+	r.PATCH("/alunos/:id", controllers.EditaAluno)
+	r.GET("/alunos/cpf/:cpf", controllers.BuscaAlunoPorCPF)
 	r.Run()
 }
